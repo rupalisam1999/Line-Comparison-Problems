e088bb281836ea2c0c233c1017974d25ae290ab6
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-15 17:52:31 +0530

    Added: Checking equality of two lines

--- Program.cs
+++ Program.cs
@@ -13,8 +13,15 @@ namespace Line_Comparison_Problems
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+            int length1 = toFindLength(x1, y1, x2, y2);
 
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length2 = toFindLength(x1, y1, x2, y2);
+
+            Console.WriteLine(length1.Equals(length2));
         }
 
         private static int toFindLength(int x1, int y1, int x2, int y2)
