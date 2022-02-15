d1276c0711bbc78a97ae922dfd48d9c10c8f2e03
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-15 18:45:14 +0530

    compare two lines

--- Program.cs
+++ Program.cs
@@ -15,21 +15,29 @@ namespace Line_Comparison_Problems
             int y2 = Convert.ToInt32(Console.ReadLine());
             int length1 = toFindLength(x1, y1, x2, y2);
 
+
+            Console.WriteLine(length1);
+
             x1 = Convert.ToInt32(Console.ReadLine());
             x2 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
             int length2 = toFindLength(x1, y1, x2, y2);
+            Console.WriteLine(length2);
 
             Console.WriteLine(length1.Equals(length2));
+
+            Console.WriteLine("Return value (comparison) =" + length1.CompareTo(length2));
+
         }
 
         private static int toFindLength(int x1, int y1, int x2, int y2)
-            {
-                return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            }
-        
+        {
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
     }
 }
+    
+
     
 
