e4f276b08ff0a97ea65931482a4392deda424884
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-21 18:16:59 +0530

    Added: check two lines are equal, lesser, greater than each other"

--- Program.cs
+++ Program.cs
@@ -8,21 +8,36 @@ namespace Line_Comparison_Problems
 
             static void Main(string[] args)
             {
-            Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(toFindLength(x1, y1, x2, y2));
-
+            Program program = new Program();
+            Console.WriteLine("if get 0:(both are equal),if get 1:(length 1 is grater than length2)," +
+            "if get -1:(length 1 is lesser than length2) :: " + program.computeMethode());
         }
-
-        private static int toFindLength(int x1, int y1, int x2, int y2)
+        public int computeMethode()
+        {
+            int length1 = 0, length2 = 0;
+            for (int i = 1; i <= 2; i++)
             {
-                return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine("input for two lines= " + i);
+                Console.WriteLine("Enter the value of (x1)");
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value of (x2)");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value of (y1)");
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value of (y2)");
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                if (i == 1)
+                {
+                    length1 = (int)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                }
+                if (i == 2)
+                {
+                    length2 = (int)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                }
             }
-        
+            return length1.CompareTo(length2);
+        }
     }
 }
-    
+
 
